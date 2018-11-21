51e8c110513ce3ee52ea5c8977333360c10ab07b
Timothée Puentes <timothee.puentes@epitech.eu>
2018-11-21 17:07:52 +0900

    [ADD] MainMenu

---
+++ Atlas/Assets/Scripts/Menu/Main/ConnectionWidget.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu.Main
+{
+    public class ConnectionWidget : MainMenuWidget
+    {
+        [SerializeField] private InputField _username;
+        [SerializeField] private InputField _password;
+        
+        [SerializeField] private Button     _connectionButton;
+        [SerializeField] private Button     _registerButton;
+
+        #region Initialisation/Destruction
+        protected override void InitialiseWidget()
+        {
+            ActualRequestManager.OnConnectionFinished += ConnectionFinished;
+
+            _connectionButton.onClick.AddListener(Connect);
+            _username.onValueChanged.AddListener(UpdateButtonState_StringListener);
+            _password.onValueChanged.AddListener(UpdateButtonState_StringListener);
+            
+            UpdateButtonState();
+        }
+
+        private void OnDestroy()
+        {
+            ActualRequestManager.OnConnectionFinished -= ConnectionFinished;
+        }
+        #endregion
+
+        #region UpdateButtonState
+        /// <summary>
+        /// Enable or not the button in case the field are filled correctly
+        /// </summary>
+        protected override void UpdateButtonState()
+        {
+            _connectionButton.interactable = (_username.text.Length > 0 && _password.text.Length > 0f && ActualRequestManager.CanReceiveANewRequest);
+            _registerButton.interactable = ActualRequestManager.CanReceiveANewRequest;
+        }
+        #endregion
+        
+        #region Request
+        /// <summary>
+        /// Call the request manager and the good function
+        /// </summary>
+        public void Connect()
+        {
+            ActualRequestManager.Connect(_username.text, _password.text);
+            UpdateButtonState();
+        }
+        private void ConnectionFinished(bool success, string message)
+        {
+            UpdateButtonState();
+            
+            if (success)
+            {
+                Debug.Log("CONNECTED");
+                //
+                // ToDo: Go to next Scene
+                //
+            }
+            else
+                ErrorText.text = message;
+        }
+        #endregion
+    }
+}
---
+++ Atlas/Assets/Scripts/Menu/Main/RegisterWidget.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu.Main
+{
+    public class RegisterWidget : MainMenuWidget
+    {
+        [SerializeField] private InputField _username;
+        [SerializeField] private InputField _emailAddress;
+        [SerializeField] private InputField _password;
+        [SerializeField] private InputField _passwordConfirmation;
+        
+        [SerializeField] private Button     _registerButton;
+        [SerializeField] private Button     _returnButton;
+    
+        #region Initialisation/Destruction
+        protected override void InitialiseWidget()
+        {
+            ActualRequestManager.OnRegisterFinished += RegisterFinished;
+
+            _registerButton.onClick.AddListener(Register);
+            _username.onValueChanged.AddListener(UpdateButtonState_StringListener);
+            _password.onValueChanged.AddListener(UpdateButtonState_StringListener);
+            _passwordConfirmation.onValueChanged.AddListener(UpdateButtonState_StringListener);
+            
+            UpdateButtonState();
+        }
+
+        private void OnDestroy()
+        {
+            ActualRequestManager.OnRegisterFinished -= RegisterFinished;
+        }
+        #endregion
+
+        #region UpdateButtonState
+        /// <summary>
+        /// Enable or not the button in case the field are filled correctly
+        /// </summary>
+        protected override void UpdateButtonState()
+        {
+            _registerButton.interactable = (_username.text.Length > 0 && _emailAddress.text.Length > 0
+                                            && _password.text.Length > 0f && _password.text.Equals(_passwordConfirmation.text)
+                                            && ActualRequestManager.CanReceiveANewRequest);
+            _returnButton.interactable = ActualRequestManager.CanReceiveANewRequest;
+        }
+        #endregion
+        
+        #region Request
+        /// <summary>
+        /// Call the request manager and the good function
+        /// </summary>
+        public void Register()
+        {
+            ActualRequestManager.Register(_username.text, _emailAddress.text, _password.text);
+            UpdateButtonState();
+        }
+        private void RegisterFinished(bool success, string message)
+        {
+            UpdateButtonState();
+
+            if (success)
+            {
+                Debug.Log("REGISTERED");
+                //
+                // ToDo: Go to next Scene
+                //
+            }
+            else
+                ErrorText.text = message;
+        }
+        #endregion
+    }
+}
