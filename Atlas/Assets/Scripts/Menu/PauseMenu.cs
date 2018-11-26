b47c30f36699379cb0e111491db3049032ce4dde
Timothée Puentes <timothee.puentes@epitech.eu>
2018-11-26 19:31:56 +0900

    [ADD] Animation when oppening pausemenu [ADD] Start a disconnection request when application quit

--- Atlas/Assets/Scripts/Menu/PauseMenu.cs
+++ Atlas/Assets/Scripts/Menu/PauseMenu.cs
@@ -1,4 +1,5 @@
 ﻿using Game;
+using Networking;
 using UnityEngine;
 
 namespace Menu
@@ -18,6 +19,11 @@ namespace Menu
 		{
 			TimeManager.Instance.PauseGame(value);
 			base.Show(value);
+		}
+
+		public void QuitTheGame()
+		{
+			Application.Quit();
 		}
 	}
 }
