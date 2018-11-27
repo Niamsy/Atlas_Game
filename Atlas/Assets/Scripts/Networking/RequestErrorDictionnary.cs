258a2800c6535ff9057a914f3986aa6b39239df3
Timothée Puentes <timothee.puentes@epitech.eu>
2018-11-27 19:45:32 +0900

    [ADD] Localized error message for the request [ADD] A demo scene showned behind the main menu

---
+++ Atlas/Assets/Scripts/Networking/RequestErrorDictionnary.cs
@@ -0,0 +1,16 @@
+﻿using Localization;
+using UnityEngine;
+
+namespace Networking
+{
+	[CreateAssetMenu(fileName = "Requests error Dictionnary", menuName = "Localization/Request Error Dictionnary")]
+	public class RequestErrorDictionnary : ScriptableObject
+	{	
+		public LocalizedText		UnknowError;
+		public LocalizedText		ApiUnreachable;
+		public LocalizedText 		ApiError;
+		public LocalizedText 		AuthentificationWrongPairing;
+		public LocalizedText 		RegistrationEmailAlreadyUsed;
+		public LocalizedText 		RegistrationPasswordNotLongEnought;
+	}
+}
