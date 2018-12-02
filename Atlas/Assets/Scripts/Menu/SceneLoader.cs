90ebcf562d872054d26ea0ac31b1bbd3023f55ac
Marc Brout <marc.brout@epitech.eu>
2018-12-02 18:27:38 +0100

    Add Atlas LoadScene in main menu

---
+++ Atlas/Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string SceneToLoad;
+    public LoadSceneMode SceneMode;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(SceneToLoad, SceneMode);
+    }
+
+}
