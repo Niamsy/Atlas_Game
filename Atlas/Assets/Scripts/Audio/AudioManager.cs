1cf571e698b41f5f4d484381e5387a7a0fb9b550
Marc Brout <marc.brout@epitech.eu>
2018-12-12 16:45:24 +0100

    [ADD] Audio Pipeline, Audio Manager & Audio Events

---
+++ Atlas/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio {
+    public class AudioManager : Singleton<AudioManager> {
+
+        private AudioStore _AudioStore;
+        private AudioSource _AudioSource;
+
+        // Use this for initialization
+        void Start() {
+            _AudioStore = new AudioStore();
+            _AudioSource = GetComponent<AudioSource>();
+            if (_AudioSource == null)
+            {
+                _AudioSource = gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+            
+        }
+
+        public void Play(AudioStore.AUDIO audio)
+        {
+            _AudioStore.Store[audio].Play(_AudioSource);
+        }
+
+        public void Play(AudioStore.AUDIO audio, AudioSource audioSource)
+        {
+            if (audioSource == null)
+            {
+                Debug.LogWarning("Audio source is null");
+                return;
+            }
+            _AudioStore.Store[audio].Play(audioSource);
+        }
+    }
+}
---
+++ Atlas/Assets/Scripts/Audio/CompositeAudioEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName="Audio Events/Composite")]
+public class CompositeAudioEvent : AudioEvent
+{
+	[Serializable]
+	public struct CompositeEntry
+	{
+		public AudioEvent Event;
+		public float Weight;
+	}
+
+	public CompositeEntry[] Entries;
+
+	public override void Play(AudioSource source)
+	{
+		float totalWeight = 0;
+		foreach (CompositeEntry e in Entries)
+			totalWeight += e.Weight;
+
+		float pick = Random.Range(0, totalWeight);
+		foreach (CompositeEntry e in Entries)
+		{
+			if (pick > e.Weight)
+			{
+				pick -= e.Weight;
+				continue;
+			}
+
+			e.Event.Play(source);
+			return;
+		}
+	}
+}
