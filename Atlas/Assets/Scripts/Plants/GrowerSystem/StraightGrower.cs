b2b62e3c03ed8548dc5e650a5d74bee3dcbf0dc9
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2018-12-18 00:35:04 +0100

    Begin implement plant system

---
+++ Atlas/Assets/Scripts/Plants/GrowerSystem/StraightGrower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plants
+{
+    [CreateAssetMenu(menuName = "Plant System/Straight Growe")]
+    public class StraightGrower : GrowerSystem
+    {
+        public override void Grow()
+        {
+            
+        }
+    }
+}
---
+++ Atlas/Assets/Scripts/Plants/Resources/Resources.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plants
+{
+    [CreateAssetMenu(menuName = "Plant System/Resource")]
+    public class Resources : ScriptableObject
+    {
+
+    }
+}
