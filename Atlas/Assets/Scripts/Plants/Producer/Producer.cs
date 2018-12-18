66f05965fb993b60ae7a5a6f33ddf5510b32c2c6
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2018-12-18 13:38:52 +0100

    Finish implementing base Plant System

---
+++ Atlas/Assets/Scripts/Plants/Producer/Producer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Variables;
+
+namespace Plants
+{
+    public class Producer<T> : MonoBehaviour
+    {
+        public Resources            type;
+        protected FloatVariable     rate;
+        protected int               quantity;
+        protected Stock<T>          stock;
+        protected bool              starverd;
+        protected FloatReference    range;
+
+        public void Produce()
+        {
+
+        }
+
+        public List<T> Unload(int sub_quantity)
+        {
+            return stock.Remove(sub_quantity);
+        }
+
+        public Stock<T> GetStock()
+        {
+            return stock;
+        }
+
+        public Resources GetRecourceType()
+        {
+            return type;
+        }
+    }
+}
