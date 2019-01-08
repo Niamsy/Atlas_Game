56733183210f387bbe2d40fd8575a38494da45d1
Marc Brout <marc.brout@epitech.eu>
2019-01-08 18:57:49 +0100

    [ADD] Audio scriptableObjects can now stop sound and have a MixerGroupo

--- Atlas/Assets/Audio/Scripts/Audio.cs
+++ Atlas/Assets/Audio/Scripts/Audio.cs
@@ -5,5 +5,10 @@ namespace AtlasAudio
     public abstract class Audio : ScriptableObject
     {
         public abstract void Play(AudioSource source);
+        public void Stop(AudioSource source)
+        {
+            if (source.isPlaying)
+                source.Stop();
+        }
     }
 }
