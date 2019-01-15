68cdd9d3f9f03af36fd3e4894feff471bdf86d5d
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-01-15 14:50:16 +0100

    [Add] Implementation of Player Constrain with abstraction stuff. [Comment] Consumtion of the different constraint is not handle by the time for now because the formuas are not done yet

---
+++ Atlas/Assets/Scripts/Game/Player/Stats/Constraint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constraint<T>
+{
+    protected T max;
+    protected  T current;
+
+    T getMax()
+    {
+        return max;
+    }
+
+    T getCurrent()
+    {
+        return current;
+    }
+
+    void setCurrent(T newCurrent)
+    {
+        current = newCurrent;
+    }
+
+    void setMax(T newMax)
+    {
+        max = newMax;
+    }
+
+}
---
+++ Atlas/Assets/Scripts/Game/Player/Stats/Constraints/SleepConstraint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepConstraint : Constraint<float>
+{
+    SleepConstraint()
+    {
+        max = 100;
+        current = max;
+    }
+
+    public void Update(double deltaTime)
+    {
+        //Operate each frame on the value of the Current Constraint
+    }
+
+    public void give(float qte)
+    {
+        current += qte;
+    }
+
+    public bool consume(float qteToConsume)
+    {
+        current -= qteToConsume;
+        return isEmpty();
+    }
+    public bool isEmpty()
+    {
+        if (current <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
