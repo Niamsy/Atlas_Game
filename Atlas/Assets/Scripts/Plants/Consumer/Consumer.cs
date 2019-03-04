3e91a9b7d27c5e16dfb3973d0a666f36eaed8275
Marc Brout <marc.brout@epitech.eu>
2019-03-04 19:54:16 +0100

    [ADD] Producer detector and automatic consuption

--- Atlas/Assets/Scripts/Plants/Consumer/Consumer.cs
+++ Atlas/Assets/Scripts/Plants/Consumer/Consumer.cs
@@ -5,27 +5,40 @@ namespace Plants
 {
     public class Consumer : MonoBehaviour, IConsumer
     {
+        #region Protected Variables
         [SerializeField]
-        protected Resources        type;
+        protected Resources type;
         [SerializeField]
-        protected int              rate;
+        [Tooltip("Rate at which the plant look for producers")]
+        [MinMaxRange(0.5f, 12000f)]
+        protected float producerScanRate = 3.0f;
         [SerializeField]
-        protected int              quantity;
-        protected Stock            stocks;
+        [MinMaxRange(0.5f, 12000f)]
+        [Tooltip("Rate at which the plant consume ressources")]
+        protected float consumeRate = 1.0f;
         [SerializeField]
-        protected int              starvationTimeLimit;
-        protected bool             starverd;
+        [Tooltip("Consumption quantity of ressources per consumption tick")]
+        protected int rate;
         [SerializeField]
-        protected FloatReference   range;
+        [Tooltip("Maximum quantity of ressources that can be taken from a producer per tick")]
+        protected int quantity;
+        protected Stock stocks;
+        [SerializeField]
+        protected int starvationTimeLimit;
+        protected bool starverd;
+        [SerializeField]
+        protected FloatReference range;
+        #endregion
 
-        public void Consume()
+        #region Properties Accessors
+        public float ProducerScanRate
         {
-            stocks.Remove(rate);
+            get { return producerScanRate; }
         }
 
-        public void Load(IProducer producer, int quantity)
+        public float ConsumeRate
         {
-            stocks.Put(producer.Unload(quantity));
+            get { return consumeRate; }
         }
 
         public Stock Stocks
@@ -75,5 +88,54 @@ namespace Plants
                 return type;
             }
         }
+        #endregion
+
+        #region Public Methods
+        public void Consume()
+        {
+            stocks.Remove(rate);
+        }
+
+        public int Load(IProducer producer, int quantity)
+        {
+            return stocks.Put(producer.Unload(quantity)).Count;
+        }
+        #endregion
+
+        #region Private Methods
+        private void Awake()
+        {
+            stocks = gameObject.AddComponent<Stock>();
+        }
+
+        private void Start()
+        {
+            InvokeRepeating("DetectProducers", Random.value, ProducerScanRate);
+            InvokeRepeating("Consume", Random.Range(1f, 3f), ConsumeRate);
+        }
+
+        private void DetectProducers()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, Range, LayerMask.GetMask("Producer"));
+            Debug.Log("Current Consumer Range" + Range.Value + ": \nCurrent colliders count = " + colliders.Length);
+            int quantityTaken = 0;
+            foreach (Collider collider in colliders)
+            {
+                Producer producer = collider.GetComponent<Producer>();
+                if (producer && producer.Type == Type)
+                {
+                    quantityTaken += Load(producer, Quantity);
+                    Debug.Log("Took " + quantityTaken + " amount");
+                }
+                if (quantityTaken >= Quantity)
+                {
+                    Debug.Log("Took everything I could");
+                    break;
+                }
+            }
+
+            Debug.Log("End of detection");
+        }
+        #endregion
     }
 }
