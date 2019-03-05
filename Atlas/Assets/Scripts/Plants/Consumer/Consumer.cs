180a8ffa3b3fd22aef77e449e4a32abf3bf09fb3
Marc Brout <marc.brout@epitech.eu>
2019-03-05 19:32:18 +0100

    [ADD] Plants automatic growth and death

--- Atlas/Assets/Scripts/Plants/Consumer/Consumer.cs
+++ Atlas/Assets/Scripts/Plants/Consumer/Consumer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using Variables;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Plants
 {
@@ -7,127 +8,175 @@ namespace Plants
     {
         #region Protected Variables
         [SerializeField]
-        protected Resources type;
+        protected Resources _type;
         [SerializeField]
         [Tooltip("Rate at which the plant look for producers")]
         [MinMaxRange(0.5f, 12000f)]
-        protected float producerScanRate = 3.0f;
+        protected float _producerScanRate = 3.0f;
         [SerializeField]
         [MinMaxRange(0.5f, 12000f)]
         [Tooltip("Rate at which the plant consume ressources")]
-        protected float consumeRate = 1.0f;
+        protected float _consumeRate = 1.0f;
         [SerializeField]
         [Tooltip("Consumption quantity of ressources per consumption tick")]
-        protected int rate;
+        protected int _rate;
         [SerializeField]
         [Tooltip("Maximum quantity of ressources that can be taken from a producer per tick")]
-        protected int quantity;
-        protected Stock stocks;
+        protected int _quantity;
+        protected Stock _stock;
         [SerializeField]
-        protected int starvationTimeLimit;
-        protected bool starverd;
+        protected int _starvationTimeLimit;
+        protected bool _starved;
         [SerializeField]
-        protected FloatReference range;
+        protected float _range;
         #endregion
 
         #region Properties Accessors
         public float ProducerScanRate
         {
-            get { return producerScanRate; }
+            get { return _producerScanRate; }
         }
 
         public float ConsumeRate
         {
-            get { return consumeRate; }
+            get { return _consumeRate; }
         }
 
-        public Stock Stocks
+        public Stock Stock
         {
-            get { return stocks; }
+            get { return _stock; }
         }
 
         public int Rate
         {
-            get { return rate; }
+            get { return _rate; }
 
-            set { rate = value; }
+            set { _rate = value; }
         }
 
-        public bool Starverd
+        public bool Starved
         {
-            get { return starverd; }
+            get { return _starved; }
 
-            set { starverd = value; }
+            set { _starved = value; }
         }
 
         public int StarvationTimeLimit
         {
-            get { return starvationTimeLimit; }
+            get { return _starvationTimeLimit; }
 
-            set { starvationTimeLimit = value; }
+            set { _starvationTimeLimit = value; }
         }
 
         public int Quantity
         {
-            get { return stocks.GetCount(); }
+            get { return _quantity; }
 
-            set { quantity = value; }
+            set { _quantity = value; }
         }
 
-        public FloatReference Range
+        public float Range
         {
-            get { return range; }
+            get { return _range; }
 
-            set { range = value; }
+            set { _range = value; }
         }
 
         public Resources Type
         {
             get
             {
-                return type;
+                return _type;
             }
         }
         #endregion
 
         #region Public Methods
+        public void Initialize(Plant.PlantStatistics statitics, Stage.Need need)
+        {
+            if (IsInvoking("DetectProducers"))
+            {
+                CancelInvoke("DetectProducers");
+            }
+            if (IsInvoking("Consume"))
+            {
+                CancelInvoke("Consume");
+            }
+
+            GrowerSystem.GrowthRate rates = statitics.GrowthRate;
+
+            _type = need.type;
+            _consumeRate = rates.ConsumationRate;
+            _producerScanRate = rates.ProducerScanRate;
+            _rate = rates.QuantityConsumed;
+            _quantity = rates.QuantityTaken;
+            _range = statitics.MaxHeight * 3;
+            _starvationTimeLimit = 60;
+            _starved = false;
+            _stock.SetLimit(need.quantity * 2);
+        }
+
+        public void StartInvoking()
+        {
+            InvokeRepeating("DetectProducers", Random.value, ProducerScanRate);
+            InvokeRepeating("Consume", Random.Range(1f, 3f), ConsumeRate);
+        }
+
         public void Consume()
         {
-            stocks.Remove(rate);
+            _stock.Remove(_rate);
+            if (_stock.GetCount() == 0)
+            {
+                StartCoroutine("Starving");
+            }
         }
 
         public int Load(IProducer producer, int quantity)
         {
-            return stocks.Put(producer.Unload(quantity)).Count;
+            List<Resources> resources = producer.Unload(quantity);
+            int count = resources.Count;
+            _stock.Put(resources);
+            resources.Clear();
+            if (count > 0)
+            {
+                StopCoroutine("Starving");
+            }
+            return count;
         }
         #endregion
 
         #region Private Methods
         private void Awake()
         {
-            stocks = gameObject.AddComponent<Stock>();
+            _stock = gameObject.AddComponent<Stock>();
         }
 
         private void Start()
         {
-            InvokeRepeating("DetectProducers", Random.value, ProducerScanRate);
-            InvokeRepeating("Consume", Random.Range(1f, 3f), ConsumeRate);
+            StartInvoking();
+        }
+
+        private IEnumerator Starving()
+        {
+            yield return new WaitForSeconds(_starvationTimeLimit);
+            _starved = true;
         }
 
         private void DetectProducers()
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, Range, LayerMask.GetMask("Producer"));
-            Debug.Log("Current Consumer Range" + Range.Value + ": \nCurrent colliders count = " + colliders.Length);
-            int quantityTaken = 0;
+            Debug.Log("Current Consumer Range" + Range + ": \nCurrent colliders count = " + colliders.Length);
+            int quantityRemaining = _quantity;
             foreach (Collider collider in colliders)
             {
                 Producer producer = collider.GetComponent<Producer>();
                 if (producer && producer.Type == Type)
                 {
-                    quantityTaken += Load(producer, Quantity);
-                    Debug.Log("Took " + quantityTaken + " amount");
+                    Debug.Log("Trying to find " + quantityRemaining + " amount");
+                    quantityRemaining -= Load(producer, quantityRemaining);
+                    Debug.Log("Took " + (_quantity - quantityRemaining) + " amount");
                 }
-                if (quantityTaken >= Quantity)
+                if (quantityRemaining <= 0)
                 {
                     Debug.Log("Took everything I could");
                     break;
---
+++ Atlas/Assets/Scripts/Plants/GrowerSystem/GrowthRate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plants.GrowerSystem
+{
+    [CreateAssetMenu(menuName = "Plant System/Growth Rate")]
+    public class GrowthRate : ScriptableObject
+    {
+        [Header("Production rates")]
+        public float ProducerScanRate;
+        public int QuantityTaken;
+        [Header("Consumption rates")]
+        public float ConsumationRate;
+        public int QuantityConsumed;
+    }
+}
