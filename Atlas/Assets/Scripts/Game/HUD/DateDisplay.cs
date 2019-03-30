470d81b3e1bdee4331b7b7d35467044e1aa48f1d
Timothée Puentes <timothee.puentes@epitech.eu>
2019-03-30 22:46:21 +0900

    [FIX] Ratio issue with the interface, and issue with the code

---
+++ Atlas/Assets/Scripts/Game/HUD/DateDisplay.cs
@@ -0,0 +1,23 @@
+﻿using Game.DayNight;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.HUD
+{
+    public class DateDisplay : MonoBehaviour
+    {
+        [SerializeField] private Text _text;
+        [SerializeField] private LocalizedTextArray _months;
+        private CalendarManager _calendar;
+
+        private void Start()
+        {
+            _calendar = CalendarManager.Instance;
+        }
+
+        private void Update()
+        {
+            _text.text = _calendar.Day.ToString("00") + " " + _months.Entries[_calendar.Month];
+        }
+    }
+}
