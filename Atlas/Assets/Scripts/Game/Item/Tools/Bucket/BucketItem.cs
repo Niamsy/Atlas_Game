64c99a2f8ee168cb1095bc81dfc8b4154bb8a587
Timothée Puentes <timothee.puentes@epitech.eu>
2019-04-03 11:22:25 +0900

    [FIX] Solved issue with Bucket

--- Atlas/Assets/Scripts/Game/Item/Tools/Bucket/BucketItem.cs
+++ Atlas/Assets/Scripts/Game/Item/Tools/Bucket/BucketItem.cs
@@ -6,7 +6,7 @@ namespace Game.Item.Tools.Bucket
 {
 	[Serializable,
 	CreateAssetMenu(fileName = "Bucket", menuName = "Item/Tool/Bucket", order = 1)]
-	public class Bucket : Tool<BucketBehaviour>
+	public class BucketItem : Tool<BucketBehaviour>
 	{
 		
         public override bool CanUse(Transform transform)
