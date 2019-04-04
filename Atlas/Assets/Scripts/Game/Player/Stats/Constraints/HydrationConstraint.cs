0e696d75391baf051df390e4149e4b403cf2c047
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-04-04 16:41:30 +0200

    Revert To 83565540bc9d8aadc3a38b851f9a785ecc4667c5

--- Atlas/Assets/Scripts/Game/Player/Stats/Constraints/HydrationConstraint.cs
+++ Atlas/Assets/Scripts/Game/Player/Stats/Constraints/HydrationConstraint.cs
@@ -8,7 +8,7 @@
 
     public void Update(double deltaTime)
     {
-        consume((float)2);
+        consume((float)0.06);
         //Operate each frame on the value of the Current Constraint
     }
 
@@ -22,10 +22,6 @@
         if (current > 0)
         {
             current -= qteToConsume;
-        }
-        if (isEmpty())
-        {
-            
         }
         return isEmpty();
     }
