eca4c14acc36f3d7732d39021dd0d775be694f3f
Marc Brout <marc.brout@epitech.eu>
2019-04-10 19:34:50 +0200

    [ADD] XP system

---
+++ Atlas/Assets/Scripts/Leveling/PlayerExperience.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leveling {
+    public class PlayerExperience : Experience
+    {
+        protected override int CalculateNextLevelXPNeeded(int NextLevel, int CurrentXPNeed)
+        {
+            return (NextLevel * NextLevel * 100);
+        }
+    }
+}
