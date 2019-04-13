cf8f69dfa9b28365f869a053109140c75140e76f
Timothée Puentes <timothee.puentes@epitech.eu>
2019-04-13 16:11:31 +0900

    [ADD] Experience bar now display level
    [ADD] Follow level

--- Atlas/Assets/Scripts/Game/HUD/ExperienceBar.cs
+++ Atlas/Assets/Scripts/Game/HUD/ExperienceBar.cs
@@ -1,6 +1,7 @@
 ﻿using Game.HUD.Commons;
 using Leveling;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.HUD
 {
@@ -9,10 +10,13 @@ namespace Game.HUD
     {
         #region Variables
         #region ObjReferences
+        [Header("Float value")]
+        [SerializeField] private Variables.FloatVariable _experience;
+        [SerializeField] private Variables.FloatVariable _level;
+        [SerializeField] private Variables.FloatVariable _levelRoof;
         [Header("Objects references")]
         [SerializeField] private HUDJauge _jauge;
-        [SerializeField] private Variables.FloatVariable _experience;
-        [SerializeField] private Variables.FloatVariable _levelRoof;
+        [SerializeField] private Text _levelDisplay;
         [SerializeField] private LevelingEvent _gainXp;
         private Animator _animator;
         #endregion
@@ -21,9 +25,12 @@ namespace Game.HUD
         [Header("Parameters variables")]
         [Tooltip("Number of seconds after the bar finished loading we should keep the display on")]
         public float DisplayLength = 2f;
+        [Tooltip("{0} Contains the int of the actual level")]
+        public string LevelDisplayFormat = "Lv. {0}";
         #endregion
 
         #region Private variables
+        private float _lastKnowLevel = 0;
         private float _timeBeforeFadeOut = 0;
         private bool _isDisplaying;
         private readonly int _hashDisplay = Animator.StringToHash("Display");
@@ -37,11 +44,17 @@ namespace Game.HUD
             _gainXp.RegisterListener(this);
             // TODO: Can't access Max Exp per level now
             _jauge.Initialize(_experience.Value, _levelRoof.Value);
+            _lastKnowLevel = _level.Value;
+            _levelDisplay.text = string.Format(LevelDisplayFormat, _level.Value);
         }
 
         public void OnEventRaised(int currentXp, int xpGain)
         {
-            _jauge.SetValue(_experience.Value);
+            if (_level.Value != _lastKnowLevel)
+                _jauge.Initialize(_experience.Value, _levelRoof.Value);
+            else
+                _jauge.SetValue(_experience.Value);
+            _levelDisplay.text = string.Format(LevelDisplayFormat, _level.Value);
             Display();
         }
 
