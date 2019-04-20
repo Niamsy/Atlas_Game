5ba64ae58f4099b8e493a9a9e5c92a681c88dc36
Marc Brout <marc.brout@epitech.eu>
2019-04-20 16:54:41 +0200

    Added XP events

---
+++ Atlas/Assets/Scripts/Leveling/LevelEventTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leveling
+{
+    public class LevelEventTrigger : MonoBehaviour
+    {
+        public LevelingEvent _LevelingEvent;
+        public int _XPQuantity = 0;
+        public int _Factor = 1;
+
+        private void OnDestroy()
+        {
+            _LevelingEvent.Raise(_XPQuantity, _Factor);
+        }
+    }
+}
