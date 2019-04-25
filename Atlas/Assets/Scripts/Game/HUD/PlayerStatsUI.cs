6c243b05fd0cb0263d3b7bcebce50b0a28a6335c
Timothée Puentes <timothee.puentes@epitech.eu>
2019-04-25 19:04:54 +0900

    [FIX] Update the HUD

--- Atlas/Assets/Scripts/Game/HUD/PlayerStatsUI.cs
+++ Atlas/Assets/Scripts/Game/HUD/PlayerStatsUI.cs
@@ -1,43 +1,47 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using Game.HUD.Commons;
+using UnityEngine;
 
-public class PlayerStatsUI : MonoBehaviour
+namespace Game.HUD
 {
-    public Image currentHealthBar;
-    public Image currentHungerBar;
-    public Image currentSleepBar;
-    public Image currentOxygenBar;
-    public Image currentStaminaBar;
-    public Image currentWaterBar;
-
-    public PlayerStats stats;
-    
-    private void Update()
+    public class PlayerStatsUI : MonoBehaviour
     {
-        UpdateBar();
-    }
+        public HUDJauge CurrentHealthBar;
+        public HUDJauge CurrentOxygenBar;
 
-    private void UpdateBar()
-    {
-        /*float ratio = stats.playerHealth.getCurrent() / stats.playerHealth.getMax();
+        public HUDJauge CurrentHungerBar;
+        public HUDJauge CurrentSleepBar;
+        public HUDJauge CurrentStaminaBar;
+        public HUDJauge CurrentWaterBar;
 
-        currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        public PlayerStats Stats;
 
+
+        private void Start()
+        {
+            CurrentHealthBar.Initialize(Stats.playerHealth.getCurrent(), Stats.playerHealth.getMax());
+            var oxygenStock = Stats._consumer.LinkedStock[Game.ResourcesManagement.Resource.Oxygen];
+            CurrentOxygenBar.Initialize(oxygenStock.Quantity, oxygenStock.Limit);
+        }
+
+        private void Update()
+        {
+            CurrentHealthBar.SetValue(Stats.playerHealth.getCurrent() / Stats.playerHealth.getMax());
+            var oxygenStock = Stats._consumer.LinkedStock[Game.ResourcesManagement.Resource.Oxygen];
+            CurrentOxygenBar.SetValue((float)oxygenStock.Quantity / (float)oxygenStock.Limit);
+
+            /*
         ratio = stats.playerHunger.getCurrent() / stats.playerHunger.getMax();
         currentHungerBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
 
         ratio = stats.playerSleep.getCurrent() / stats.playerSleep.getMax();
-        currentSleepBar.rectTransform.localScale = new Vector3(ratio, 1, 1);*/
-
-        var oxygenStock = stats._consumer.LinkedStock[Game.ResourcesManagement.Resource.Oxygen];
-        float ratio = (float)oxygenStock.Quantity / (float)oxygenStock.Limit;
-        currentOxygenBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
-
-       /* ratio = stats.playerStamina.getCurrent() / stats.playerStamina.getMax();
+        currentSleepBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratio = stats.playerStamina.getCurrent() / stats.playerStamina.getMax();
         currentStaminaBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
 
         ratio = stats.playerHydration.getCurrent() / stats.playerHydration.getMax();
-        currentWaterBar.rectTransform.localScale = new Vector3(ratio, 1, 1);*/
-    }
+        currentWaterBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        */
+        }
     
+    }
 }
