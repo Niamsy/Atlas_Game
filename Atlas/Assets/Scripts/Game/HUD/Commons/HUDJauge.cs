610294fef54fb508ad565d328d5b02c8da5a3c03
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-05-01 22:43:48 +0200

    [ADD] Sound Jauge Critical value

--- Atlas/Assets/Scripts/Game/HUD/Commons/HUDJauge.cs
+++ Atlas/Assets/Scripts/Game/HUD/Commons/HUDJauge.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using AtlasAudio;
+using UnityEngine.Events;
 
 namespace Game.HUD.Commons
 {
     public class HUDJauge : MonoBehaviour
     {
+        #region Custom Events
+        public UnityEvent OnCriticalJaugeValue;
+        public UnityEvent OnStableJaugeValue;
+        #endregion
+
         [Header("Objects references")]
         [SerializeField] private Image _fill;
         [SerializeField] private Image _prefill;
@@ -18,6 +24,9 @@ namespace Game.HUD.Commons
         public float EmptyingSpeed = 2f;
         [Tooltip("See string.Format function, {0} in the actual value, {1} is the max value, {2} is the percentage")]
         public string TextFormat = "{0:F2} / {1:F2}";
+        [Range(0, 1)]
+        [Tooltip("Float representing the critical content value of the jauge when a sound will be emmited")]
+        public float ThresholdCriticalValue = 0.5f;
 
         [Header("Sounds")]
         [SerializeField] private SimpleAudio _fillAudio;
@@ -26,6 +35,7 @@ namespace Game.HUD.Commons
         private float _targetValue;
         private float _value;
         private float _maxValue;
+        private bool _isValueCritical;
         
         private float ActualPercentage01
         {
@@ -45,6 +55,7 @@ namespace Game.HUD.Commons
         {
             if (_source)
                 _source.loop = true;
+            _isValueCritical = false;
         }
         
         public void Initialize(float value, float maxValue)
@@ -84,6 +95,19 @@ namespace Game.HUD.Commons
                 else
                     UpdateValue(Mathf.MoveTowards(_value, _targetValue, (EmptyingSpeed * _maxValue) * Time.deltaTime));
                 UpdateText();
+            }
+            float percentageValue = ((_value * 100) / _maxValue) / 100;
+            Debug.Log(percentageValue);
+            if (percentageValue <= ThresholdCriticalValue && OnCriticalJaugeValue != null && _isValueCritical == false)
+            {
+                OnCriticalJaugeValue.Invoke();
+                Debug.Log("SOUND");
+                _isValueCritical = true;
+            }
+            else if (percentageValue > ThresholdCriticalValue && OnCriticalJaugeValue != null && _isValueCritical == true)
+            {
+                OnStableJaugeValue.Invoke();
+                _isValueCritical = false;
             }
 
             if (_targetValue == _value && _fillAudio && _source)
