6f815a99594f62336c398a3a3a02273c57c8f7c2
Myriam <myriam.cholet@epitech.eu>
2019-05-05 16:39:07 +0200

    [ADD] game over screen

---
+++ Atlas/Assets/Player/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Vector3))]
+
+public class Spawner : MonoBehaviour
+{
+    public Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        Debug.Log("Spawn");
+        gameObject.transform.position = pos;
+    }
+}
---
+++ Atlas/Assets/Scripts/Menu/GameOverMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using SceneManagement;
+using Player;
+
+namespace Menu
+{
+    [RequireComponent(typeof(PlayerController))]
+    public class GameOverMenu : MenuWidget
+    {
+        public PlayerController _playerController;
+        private PlayerStats      _playerStats;
+        private Canvas           _canvas;
+        private Spawner          _spawner;
+        private bool _isAlreadyDead;
+        protected override void InitialiseWidget()
+        {
+            
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _canvas = GetComponent<Canvas>();
+            _playerStats = _playerController.GetComponentInChildren<PlayerStats>();
+            _spawner = _playerController.GetComponentInChildren<Spawner>();
+            Show(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_playerController.IsDead && !_isAlreadyDead)
+            {
+                _isAlreadyDead = true;
+                Show(true);
+            }
+        }
+
+        public void Quit()
+        {
+            SceneLoader.Instance.QuitTheGame();
+        }
+
+        public void ContinueGame()
+        {
+            _playerStats._consumer.LinkedStock[Game.ResourcesManagement.Resource.Oxygen].Quantity = _playerStats._consumer.LinkedStock[Game.ResourcesManagement.Resource.Oxygen].Limit;
+            _spawner.Spawn();
+            _isAlreadyDead = false;
+            Show(false);
+        }
+
+        public override void Show(bool display, bool force = false)
+        {
+            Debug.Log(display);
+            base.Show(display, force);
+        }
+    }
+}
