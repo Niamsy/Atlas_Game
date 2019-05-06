d433f3f16731b430334bee14327a0991c31a6424
Timothée Puentes <timothee.puentes@epitech.eu>
2019-05-06 19:55:18 +0900

    [UPDATE] The animation of watering

---
+++ Atlas/Assets/Scripts/Tools/KeepInRotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+    [ExecuteInEditMode]
+    public class KeepInRotation : MonoBehaviour
+    {
+        public bool FreezeX;
+        public bool FreezeY;
+        public bool FreezeZ;
+        
+        public Vector3 Rotation;
+
+        public void Update()
+        {
+            var actual = transform.rotation.eulerAngles;
+            var rotation = Rotation;
+            if (FreezeX)
+                rotation.x = actual.x;
+            if (FreezeY)
+                rotation.y = actual.y;
+            if (FreezeZ)
+                rotation.z = actual.z;
+            
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
