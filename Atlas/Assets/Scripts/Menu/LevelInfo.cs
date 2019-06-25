5167c0406c7bf9229c68afb51389c51624dfb774
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-06-25 15:37:35 +0200

    [ADD] Begin Implement Level Selector Interface

---
+++ Atlas/Assets/Scripts/Menu/LevelInfo.cs
@@ -0,0 +1,22 @@
+﻿using Localization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu.LevelSelector
+{
+    public class LevelInfo : ScriptableObject
+    {
+        [Header("LevelInfo")]
+        [SerializeField] private LocalizedText  _levelTitle = null;
+        public string                           LevelTitle => (_levelTitle.name);
+        [SerializeField] private string         _timePlayed = null;
+        public string                           TimePlayed => (_timePlayed);
+        [SerializeField] private LocalizedText  _levelDescription = null;
+        public string LevelDescription => (_levelDescription.name);
+
+        [Header("Level")]
+        [SerializeField] private int            _challengeOnThisLevelComplete = 0;
+        public int                              ChallengeOnThisLevelComplete => (_challengeOnThisLevelComplete);
+    }
+}
