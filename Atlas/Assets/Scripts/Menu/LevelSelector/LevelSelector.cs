bf4b84af13f712107cee80efa5b72b72d9d59e2a
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-06-29 18:39:46 +0200

    [ADD] Advance on implement level selector

--- Atlas/Assets/Scripts/Menu/LevelSelector/LevelSelector.cs
+++ Atlas/Assets/Scripts/Menu/LevelSelector/LevelSelector.cs
@@ -6,9 +6,16 @@ namespace Menu.LevelSelector
 {
     public class LevelSelector : MenuWidget
     {
+        private LevelInfo       _currentLevel;
+        [SerializeField]
+        public List<LevelInfo>  Levels;
+
         protected override void InitialiseWidget()
         {
-            
+            if (Levels.Count > 0)
+            {
+                _currentLevel = Levels[0];
+            }
         }
 
         // Start is called before the first frame update
