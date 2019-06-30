135d5bfcafe070d6faf428a51f258ce8ebef791c
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-06-30 20:58:44 +0200

    [Add] Implement  feature Level Selector Dynamic script

--- Atlas/Assets/Scripts/Menu/LevelSelector/LevelSelector.cs
+++ Atlas/Assets/Scripts/Menu/LevelSelector/LevelSelector.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu.LevelSelector
 {
     public class LevelSelector : MenuWidget
     {
         private LevelInfo       _currentLevel;
+
+        [Header("Prefab Level")]
+        [SerializeField]
+        public GameObject PrefabLevel;
+
         [SerializeField]
         public List<LevelInfo>  Levels;
 
@@ -15,6 +21,42 @@ namespace Menu.LevelSelector
             if (Levels.Count > 0)
             {
                 _currentLevel = Levels[0];
+            }
+            for (int i = 0; i < Levels.Count; ++i)
+            {
+                GameObject level = Instantiate(PrefabLevel, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                level.transform.parent = GameObject.Find("ListLevels").transform;
+                level.name = "Level " + i;
+                GameObject child = level.transform.Find("Level").gameObject;
+                foreach (Transform ui in child.GetComponentsInChildren<Transform>())
+                {
+                    Debug.Log(ui.name);
+                    if (ui && ui.name == "LevelTitle")
+                    {
+                        Text levelTitle = ui.GetComponent<Text>();
+                        levelTitle.text = Levels[i].LevelTitle;
+                    }
+                    else if (ui && ui.name == "LevelDescription")
+                    {
+                        Text levelDesc = ui.GetComponent<Text>();
+                        levelDesc.text = Levels[i].LevelDescription;
+                    }
+                    else if (ui && ui.name == "PlayTime")
+                    {
+                        Text timePlayed = ui.GetComponent<Text>();
+                        timePlayed.text = Levels[i].TimePlayed;
+                    }
+                    else if (ui && ui.name == "PanelLocked")
+                    {
+                        // TODO Make General Data for this game. Like Number Challenge complete. Change this condition
+                        if (Levels[i].ChallengeOnThisLevelComplete > 0)
+                        {
+                            GameObject panel = ui.GetComponent<GameObject>();
+                            if (panel)
+                                panel.SetActive(false);
+                        }
+                    }
+                }
             }
         }
 
