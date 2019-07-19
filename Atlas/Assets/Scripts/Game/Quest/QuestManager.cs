9884636671b3bea60e05ec8c41334d55989fb792
zouz <fabian.le-maux@epitech.eu>
2019-07-19 09:40:04 +0200

    Adding Quest System

---
+++ Atlas/Assets/Scripts/Game/Quest/QuestManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quest
+{
+
+
+    public class QuestManager : Tools.Singleton<QuestManager>
+    {
+
+        public void UpdateQuestsWith(ObjType objectiveType, string Id)
+        {
+            foreach (Quest q in __quests)
+            {
+                q.UpdateQuest(objectiveType, Id);
+            }
+        }
+
+        public void addQuest(Quest q)
+        {
+            __quests.Add(q);
+        }
+
+        public void applyRewards(GameObject player)
+        {
+            List<int> idxs = new List<int>();
+            int i = 0;
+            foreach(Quest q in __quests)
+            {
+                if (q.isComplete())
+                {
+                    q.ApplyReward(player);
+                    idxs.Add(i);
+                }
+                ++i;
+            }
+            foreach (int idx in idxs)
+            {
+                __quests.RemoveAt(idx);
+            }
+        }
+
+        private List<Quest> __quests;
+    }
+}
