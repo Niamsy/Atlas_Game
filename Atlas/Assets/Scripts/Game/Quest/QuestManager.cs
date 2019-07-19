38b9133efaf8076825b1567c175e8521fddf2fb2
zouz <fabian.le-maux@epitech.eu>
2019-07-19 17:25:03 +0200

    [EDIT] Quest Improving

--- Atlas/Assets/Scripts/Game/Quest/QuestManager.cs
+++ Atlas/Assets/Scripts/Game/Quest/QuestManager.cs
@@ -1,46 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Tools;
 using UnityEngine;
 
-namespace Quest
+
+
+public class QuestManager : Singleton<QuestManager>
 {
-
-
-    public class QuestManager : Tools.Singleton<QuestManager>
+   private QuestList qlist;
+   QuestManager()
     {
-
-        public void UpdateQuestsWith(ObjType objectiveType, string Id)
-        {
-            foreach (Quest q in __quests)
-            {
-                q.UpdateQuest(objectiveType, Id);
-            }
-        }
-
-        public void addQuest(Quest q)
-        {
-            __quests.Add(q);
-        }
-
-        public void applyRewards(GameObject player)
-        {
-            List<int> idxs = new List<int>();
-            int i = 0;
-            foreach(Quest q in __quests)
-            {
-                if (q.isComplete())
-                {
-                    q.ApplyReward(player);
-                    idxs.Add(i);
-                }
-                ++i;
-            }
-            foreach (int idx in idxs)
-            {
-                __quests.RemoveAt(idx);
-            }
-        }
-
-        private List<Quest> __quests;
+        qlist = new QuestList();
+        __activeQuests = new List<Quest>();
+        __availableQuests = qlist.getQuestList();
+        __activeQuests.Add(__availableQuests[0]);
     }
+
+    public void UpdateQuestsWith(ObjType objectiveType, string Id, GameObject Player)
+    {
+        bool flag = false;
+        foreach (Quest q in __activeQuests)
+        {
+            if (q.UpdateQuest(objectiveType, Id))
+            {
+                flag = true;
+            }
+        }
+        if (flag)
+        {
+            applyRewards(Player);
+        }
+    }
+
+   public void addQuest(Quest q)
+   {
+         __activeQuests.Add(q);
+   } 
+
+   public void applyRewards(GameObject player)
+   {
+        List<int> idxs = new List<int>();
+        int i = 0;
+        foreach(Quest q in __activeQuests)
+        {
+           if (q.isComplete())
+           {
+               q.ApplyReward(player);
+               idxs.Add(i);
+           }
+           ++i;
+        }
+        foreach (int idx in idxs)
+        {
+           __activeQuests.RemoveAt(idx);
+        }
+   }
+   private List<Quest> __activeQuests;
+   private List<Quest> __availableQuests;
 }
