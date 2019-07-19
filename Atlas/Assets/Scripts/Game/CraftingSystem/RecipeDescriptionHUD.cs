35703e742b7307f6f20c657cf5c3cf95495f20e5
Marc Brout <marc.brout@epitech.eu>
2019-07-19 08:36:56 +0200

    blueprint screen working

--- Atlas/Assets/Scripts/Game/CraftingSystem/RecipeDescriptionHUD.cs
+++ Atlas/Assets/Scripts/Game/CraftingSystem/RecipeDescriptionHUD.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Game.Crafting;
+using TMPro;
 
 namespace Menu.Crafting
 {
     public class RecipeDescriptionHUD : MonoBehaviour
     {
         [SerializeField] private Image _recipeImage;
-        [SerializeField] private Text _recipeName;
-        [SerializeField] private Text _recipeDescription;
+        [SerializeField] private TextMeshProUGUI _recipeName;
+        [SerializeField] private TextMeshProUGUI _recipeDescription;
+        [SerializeField] private TextMeshProUGUI _recipeAvailability;
+
+        [Header("Availability Info")]
+        [SerializeField] private Localization.LocalizedText _availableLocalizedText;
+        [SerializeField] private Localization.LocalizedText _unavailableLocalizedText;
+        [SerializeField] private Color _availableLocalizedTextColor;
+        [SerializeField] private Color _unavailableLocalizedTextColor;
         // Update is called once per frame
 
         private Recipe _currentRecipe = null;
@@ -24,9 +32,32 @@ namespace Menu.Crafting
 
         }
 
-        public void SetRecipe(Recipe recipe = null)
+        public void Reset()
         {
+            SetRecipe();
+        }
 
+        public void SetRecipe(Transform recipTransform = null, Recipe recipe = null)
+        {
+            _currentRecipe = recipe;
+
+            UpdateDescription(recipTransform);
+        }
+
+        public void UpdateDescription(Transform recipTransform)
+        {
+            gameObject.SetActive(_currentRecipe != null);
+
+            if (_currentRecipe == null) return;
+
+            _recipeImage.sprite = _currentRecipe.Sprite;
+            _recipeName.text = _currentRecipe.Name;
+            _recipeDescription.text = _currentRecipe.Description;
+            _recipeAvailability.text = _currentRecipe.isUnlocked ? _availableLocalizedText : _unavailableLocalizedText;
+            _recipeAvailability.color = _currentRecipe.isUnlocked ? _availableLocalizedTextColor : _unavailableLocalizedTextColor;
+
+            if (recipTransform)
+                transform.SetPositionAndRotation(new Vector3(recipTransform.position.x + 32, recipTransform.position.y - 10) , recipTransform.rotation);
         }
     }
 }
