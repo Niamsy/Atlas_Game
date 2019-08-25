d0cb0692b6e0922cb219804f8f7bf6fa07e98d6c
Marc Brout <marc.brout@epitech.eu>
2019-08-25 21:35:20 +0200

    [update] setting up crafting algorithm

---
+++ Atlas/Assets/Scripts/Game/CraftingSystem/GridProducingHUD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using Game.Crafting;
+using UnityEngine;
+
+public class GridProducingHUD : MonoBehaviour
+{
+    private List<ProductHUD> _products = new List<ProductHUD>();
+    
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        _products = new List<ProductHUD>(GetComponentsInChildren<ProductHUD>());
+    }
+
+    public void Set(List<Recipe.Product> products)
+    {
+        
+    }
+
+    public void push(Recipe.Product product, int slot)
+    {
+        
+    }
+
+    public void remove(Recipe.Product product, int slot)
+    {
+        
+    }
+}
--- Atlas/Assets/Scripts/Game/CraftingSystem/Recipe.cs
+++ Atlas/Assets/Scripts/Game/CraftingSystem/Recipe.cs
@@ -2,6 +2,9 @@
 using Game.Item;
 using UnityEngine;
 using System;
+using UnityEngine.Events;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.UIElements;
 
 namespace Game
 {
@@ -27,33 +30,46 @@ namespace Game
                 public ItemAbstract Item;
                 [UnityEngine.Range(1, 999)]
                 public int ProducedQuantity;
+
+                [Serializable]
+                public class ProductEvent : UnityEvent<Product>
+                {
+                }
+                
+                private int _timeRemaining;
+                private float _lastTime;
+                private readonly UnityEvent<Product> _onEnd = new ProductEvent(); 
+                public int TimeRemaining => _timeRemaining;
+                public bool IsFinished => _timeRemaining == 0;
+
+                public void AddListenerOnEnd(UnityAction<Product> OnEndAction)
+                {
+                    _onEnd.AddListener(OnEndAction);
+                }
+                
+                public void Start(Recipe recipe)
+                {
+                    _timeRemaining = recipe.Duration;
+                    _lastTime = Time.time;
+                }
+
+                public void Update()
+                {
+                    if (_timeRemaining <= 0) return;
+                    _timeRemaining -= (int) (Time.time - _lastTime);
+                    _lastTime = Time.time;
+                    if (_timeRemaining <= 0)
+                        _onEnd.Invoke(this);
+                }
             }
             #endregion
 
             #region Public Accessors
-            public Ingredient[] Ingredients
-            {
-                get { return _Ingredients; }
-                private set { }
-            }
-
-            public Product Produced
-            {
-                get { return _Product; }
-                private set { }
-            }
-
-            public RecipeCategory Category
-            {
-                get { return _Category; }
-                private set { }
-            }
-
-            public bool isUnlocked
-            {
-                get { return _Unlocked; }
-                private set { }
-            }
+            public Ingredient[] Ingredients => _Ingredients;
+            public Product Produced => _Product;
+            public RecipeCategory Category => _Category;
+            public bool isUnlocked => _Unlocked;
+            public int Duration => CraftingDuration;
             #endregion
 
             #region Public Properties
