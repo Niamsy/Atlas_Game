c99dab3a14772cfdab657218f5133719f0fbc980
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-09-16 12:42:02 +0200

    [FIX] Fix Bug Load Level

---
+++ Atlas/Assets/Scripts/Tools/UiBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetActiveInactive(GameObject gameObject) => gameObject.SetActive((gameObject.activeSelf == true) ? false : true);
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
