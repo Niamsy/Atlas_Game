aa90eb7146b61182e5cc939121800c3024832801
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-09-27 16:05:52 +0200

    Adding Cinematique to Test scene with player & animations

---
+++ Atlas/Assets/Cinematique/PlayerCinematic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCinematic : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator.SetFloat("InputMagnitude", 0.5f);
+    }
+
+    public float speedRatio = 0.01f;
+    public IEnumerator StopPlayer()
+    {
+        var speed = 0.5f;
+        while (speed > 0)
+        {
+            animator.SetFloat("InputMagnitude", speed);
+            speed -= speedRatio;
+            yield return 0;
+        }
+    }
+}
