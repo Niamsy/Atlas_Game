dfbbaabc731d5dfea19d79786e9501304adeafcd
My-Lan Aragon <mylan.aragon@gmail.com>
2019-10-05 17:57:04 +0200

    [ADD] Rain loop through grid to give water

--- Atlas/Assets/Scripts/Game/Map/Rain/RainManager.cs
+++ Atlas/Assets/Scripts/Game/Map/Rain/RainManager.cs
@@ -1,10 +1,13 @@
 ﻿using DigitalRuby.RainMaker;
+using Game.Grid;
+using Game.ResourcesManagement.Consumer;
 using UnityEngine;
 
 public class RainManager : MonoBehaviour
 {
     public float Frequency = 0f;
     public float Duration = 0;
+    public WorldGrid WorldGrid;
 
     private float timeleft = 0;
     private float intensity = 0f;
@@ -25,7 +28,7 @@ public class RainManager : MonoBehaviour
         if (timeleft <= 0)
         {
             float rand = Random.value;
-            if (rand < Frequency)
+            if (rand <= Frequency)
             {
                 rainScript.RainIntensity = intensity;
             }
@@ -37,7 +40,40 @@ public class RainManager : MonoBehaviour
         }
         else if (timeleft > 0)
         {
+            if (WorldGrid.Grid != null)
+            {
+                GiveWater();
+            }
+            else
+            {
+                Debug.Log("Null grid");
+            }
+
             timeleft -= Time.deltaTime;
+        }
+    }
+
+    void GiveWater()
+    {
+        Debug.Log("x: " + WorldGrid.gridWorldSize.x + " y: " + WorldGrid.gridWorldSize.y);
+        for (var x = 0; x < WorldGrid.gridWorldSize.x; x++)
+        {
+            for (var y = 0; y < WorldGrid.gridWorldSize.y; y++)
+            {
+                if (WorldGrid.Grid[x, y] == null)
+                    continue;
+
+                var plant = WorldGrid.Grid[x, y].Plant;
+                if (plant == null)
+                    continue;
+
+                var consumer = WorldGrid.Grid[x, y].Plant.GetComponent<PlantConsumer>();
+                if (consumer != null)
+                {
+                    Debug.Log("Consumer not null, give to " + WorldGrid.Grid[x, y].Plant.name);
+                    consumer.ReceiveResource(Game.ResourcesManagement.Resource.Water, (int)(rainScript.RainIntensity * 10));
+                }
+            }
         }
         
     }
