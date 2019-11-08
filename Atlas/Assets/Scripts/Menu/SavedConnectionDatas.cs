2c3b583a95dd7bf1ed0d2d1e83bbffdf1f5fdf57
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-11-08 14:06:29 +0100

    Prepare Persistent Id in Connection

---
+++ Atlas/Assets/Scripts/Menu/SavedConnectionDatas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SavedConnectionDatas", menuName = "Menu/SavedConnection", order = 2)]
+public class SavedConnectionDatas : ScriptableObject
+{
+    private string IdConnection;
+
+    public string getId()
+    {
+        return IdConnection;
+    }
+
+    public void setId(string newId)
+    {
+        IdConnection = newId;
+    }
+}
