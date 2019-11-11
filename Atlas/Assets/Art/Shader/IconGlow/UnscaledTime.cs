7f3216750042479701bc484ff521aa00b0bf7f7a
Marc Brout <marc.brout@epitech.eu>
2019-11-11 02:12:28 +0100

    [ADD] Icon Glow Shader Effect

---
+++ Atlas/Assets/Art/Shader/IconGlow/UnscaledTime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnscaledTime : MonoBehaviour
+{
+    [SerializeField] private string timeProperty = "Vector1_E78B2588";
+
+    private Material material = null;
+
+    private float RandomValue = 0f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        var image = GetComponent<Image>();
+        material = image.material;
+        RandomValue = Random.Range(0f, 10000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.SetFloat(timeProperty, Time.unscaledTime + RandomValue);
+    }
+    
+}
