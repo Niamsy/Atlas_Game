6914f3814cd91b0592d73acf2ce1f1adea30c7f7
Fabian le Maux <fabian.lemaux@hotmail.fr>
2019-11-14 11:01:08 +0100

    Patch Cinematic & connection Menu

--- Atlas/Assets/Scripts/Menu/SavedIdHandler.cs
+++ Atlas/Assets/Scripts/Menu/SavedIdHandler.cs
@@ -8,6 +8,8 @@ public class SavedIdHandler : MonoBehaviour
 {
     [SerializeField] SavedConnectionDatas _data;
     [SerializeField] InputField _field;
+    [SerializeField] InputField _fieldMdp;
+
     public bool _save;
 
     private void Start()
@@ -16,16 +18,33 @@ public class SavedIdHandler : MonoBehaviour
         {
             _field.text = _data.getId();
         }
+        if (_data.getPasswd().Length > 0)
+        {
+            _fieldMdp.text = _data.getPasswd();
+        }
     }
 
     public void setSave(bool value)
     {
+        print("Set Save:" + value);
         _save = value;
+    }
+
+    public void setPasswd(string pass)
+    {
+        print("Set Passwd:" + pass);
+        _data.setPassword(pass);
     }
 
     public void setId(string id)
     {
+        print("Set Id");
         _data.setId(id);
+    }
+
+    public string getSavePass()
+    {
+        return _data.getPasswd();
     }
 
     public string getSavedId()
