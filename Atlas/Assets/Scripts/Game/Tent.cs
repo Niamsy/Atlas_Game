567cf3b44c026e3b19949acf7312da88a58e2314
DUHIEU Benjamin <benjamin.duhieu@epitech.eu>
2019-12-05 15:53:49 +0100

    feat: Update message usage when Player close to an interactable object

--- Atlas/Assets/Scripts/Game/Tent.cs
+++ Atlas/Assets/Scripts/Game/Tent.cs
@@ -9,6 +9,13 @@ public class Tent : AInteractable
 {
     public Image blackscreen;
     private bool fading = false;
+
+    private void Awake()
+    {
+        if (_hidedCanvasName != null && _hidedCanvasUsage != null)
+            triggerCanvas(false);
+    }
+
     public override void Interact(PlayerController playerController)
     {
         if (fading == false)
@@ -17,6 +24,39 @@ public class Tent : AInteractable
             var energy = playerController.GetPlayerStats().Resources[Resource.Energy];
             energy.Quantity = energy.Limit;
             var corout = StartCoroutine(Fade());
+        }
+    }
+
+    private void triggerCanvas(bool visible)
+    {
+        Color colName = _hidedCanvasName.color;
+        Color colUsage = _hidedCanvasUsage.color;
+        colName.a = 0;
+        colUsage.a = 0;
+        if (visible)
+        {
+            colName.a = 255;
+            colUsage.a = 255;
+        }
+        _hidedCanvasUsage.color = colUsage;
+        _hidedCanvasName.color = colName;
+    }
+
+    protected override void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (_hidedCanvasName != null && _hidedCanvasUsage != null)
+                triggerCanvas(true);
+        }
+    }
+
+    protected override void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (_hidedCanvasName != null && _hidedCanvasUsage != null)
+                triggerCanvas(false);
         }
     }
 
