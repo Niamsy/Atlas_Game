edd50b166101607eff6a99c837d4c74dccd67a4b
zouz <fabian.le-maux@epitech.eu>
2019-12-05 20:51:03 +0100

    Adding Cinematique prefab & Render to Build

---
+++ Atlas/Assets/Cinematique/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    float TimerTransition = 18.0f;
+    float ElapsedTime = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElapsedTime += Time.deltaTime;
+        if (ElapsedTime > TimerTransition)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
